11dce60657b9bd89605d4f831acb1f7824206bc7
Reesa7 <109600014+Reesa7@users.noreply.github.com>
2022-07-31 22:37:11 +0530

    Add files via upload

---
+++ Ascending.java
@@ -0,0 +1,23 @@
+package ascending;
+
+import java.io.*;
+import java.util.*;
+
+public class Ascending {
+	
+	static String directory= "src/assistedproject4";
+	public static void ascendingOrder() {
+		
+		File[] files = new File(directory).listFiles();
+		Set<String> a = new TreeSet<>();
+		for(File file : files) {
+			if (!file.isFile()) {
+				continue;
+			}
+			a.add(file.getName());
+		}
+		a.forEach(i->System.out.println(i));
+
+	}
+}
+
